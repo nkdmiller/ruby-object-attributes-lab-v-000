de78078428633ce672f985be1aee85d67e64f26d
Nate Miller <natekmiller96@gmail.com>
2018-06-15 21:52:18 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  def initialize(breed, name)
+    @breed = breed
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+    
